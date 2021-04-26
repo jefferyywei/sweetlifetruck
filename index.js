39c3692a5eedbadf52545ed58aabf25dcf256c38
BotDestroyer <59812447+BotDestroyer@users.noreply.github.com>
2021-04-25 20:31:39 -0400

    finished homepage fr

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+$( document ).ready(function() {
+    $.fn.isInViewport = function() {
+        var elementTop = $('.navimage').offset().top;
+        var elementBottom = elementTop + $('.navimage').outerHeight();
 
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    $(window).on('resize scroll', function() {
+        if ($('.navimage').isInViewport()) {
+            $( "div.navbar-2" ).addClass( "hide" )
+        } else {
+            $( "div" ).removeClass( "hide" )
+        }
+    });
+});
