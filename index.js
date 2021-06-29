348a30cd7a3c316051b5aa592607ef6b29252985
BotDestroyer <59812447+BotDestroyer@users.noreply.github.com>
2021-06-29 14:34:41 -0400

    made home page mobile

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("link-container");
+    if (x.className === "link-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "link-container";
+    }
+
+    var y = document.getElementById("overlay-wrapper");
+    if (y.className === "overlay-wrapper") {
+        y.className += " show";
+    } else {
+        y.className = "overlay-wrapper";
+    }
+}
+
 $(document).ready(function(){
     $.fn.isInViewport = function() {
         var elementTop = $('.navbar').offset().top;
