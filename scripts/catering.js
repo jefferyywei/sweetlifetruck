2f55c5a4c39f970221d143a910b4df96873ad476
BotDestroyer <59812447+BotDestroyer@users.noreply.github.com>
2022-03-02 14:46:53 -0500

    Added proper gallery images
    Removed extra comments
    Tweaked height of images

--- scripts/catering.js
+++ scripts/catering.js
@@ -1,23 +1,3 @@
-// $(document).ready(function(){
-//     $.fn.isInViewport = function() {
-//         var elementTop = $('.navbar').offset().top;
-//         var elementBottom = elementTop + $('.navbar').outerHeight();
-//
-//         var viewportTop = $(window).scrollTop();
-//         var viewportBottom = viewportTop + $(window).height();
-//
-//         return elementBottom > viewportTop && elementTop < viewportBottom;
-//     };
-//
-//     $(window).on('resize scroll', function() {
-//         if ($('.navbar').isInViewport()) {
-//             $( "div.navbar-2" ).addClass( "hide" )
-//         } else {
-//             $( "div" ).removeClass( "hide" )
-//         }
-//     });
-// });
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("link-container");
--- scripts/menu.js
+++ scripts/menu.js
@@ -21,24 +21,6 @@ function scroll_about(){
 }
 
 $(document).ready(function(){
-    // $.fn.isInViewport = function() {
-    //     var elementTop = $('.navbar').offset().top;
-    //     var elementBottom = elementTop + $('.navbar').outerHeight();
-    //
-    //     var viewportTop = $(window).scrollTop();
-    //     var viewportBottom = viewportTop + $(window).height();
-    //
-    //     return elementBottom > viewportTop && elementTop < viewportBottom;
-    // };
-
-    // $(window).on('resize scroll', function() {
-    //     if ($('.navbar').isInViewport()) {
-    //         $( "div.navbar-2" ).addClass( "hide" )
-    //     } else {
-    //         $( "div" ).removeClass( "hide" )
-    //     }
-    // });
-
     $.fn.isInViewport = function() {
         var elementTop = $('.navbar').offset().top;
         var elementBottom = elementTop + $('.navbar').outerHeight();
